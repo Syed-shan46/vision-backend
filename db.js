c1f28d459b4c1b8dfd3c60307d75105b8f864c92
Syed S <syedshan093@gmail.com>
2025-09-03 15:16:05 +0530

    project hosted on render

--- db.js
+++ db.js
@@ -3,12 +3,21 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 // Create PostgreSQL connection pool
+// const pool = new Pool({
+//   user: process.env.DB_USER || "postgres",
+//   host: process.env.DB_HOST || "localhost",
+//   database: process.env.DB_NAME || "vision",
+//   password: process.env.DB_PASSWORD || "syed123",
+//   port: process.env.DB_PORT || 5432,
+// });
+
+const connectionString = process.env.DATABASE_URL || process.env.LOCAL_DATABASE_URL;
+
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "vision",
-  password: process.env.DB_PASSWORD || "syed123",
-  port: process.env.DB_PORT || 5432,
+  connectionString,       // <- uses Render env variable
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false }                 // needed for cloud DB
+    : false,
 });
 
 // Test DB connection once
